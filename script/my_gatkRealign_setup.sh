d0bf7eff8a990b934a50ac3df285eb33fa77859d
Kenichi Chiba <kchiba@hgc.jp>
2012-05-10 15:30:03 +0900

    commit gatk set up tools

---
+++ script/my_gatkRealign_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+
+#  Copyright Human Genome Center, Institute of Medical Science, the University of Tokyo
+#  @since 2012
+
+GENREF=$1
+GATK=$2
+JAVA=$3
+TMP=$4
+SCRIPTDIR=$5
+
+source ${SCRIPTDIR}/utility.sh
+
+
+echo Realign reads around target intervals
+${JAVA} -Xms6g -Xmx6g -Djava.io.tmpdir=${TMP} -jar ${GATK}/GenomeAnalysisTK.jar \
+  -T IndelRealigner \
+  -targetIntervals ../install/gatksetup/1.interval_list \
+  -R ${GENREF} \
+  -I ../install/gatksetup/ga.bam \
+  -o ../install/gatksetup/realigned.bam \
+  -compress 0 \
+  -baq OFF
+check_error $?
+
+
+: <<'#__COMMENT_OUT__'
+#__COMMENT_OUT__
---
+++ script/realign_gatk_setup.py
@@ -0,0 +1,50 @@
+#  Copyright Human Genome Center, Institute of Medical Science, the University of Tokyo
+#  @since 2012
+
+import os
+import subprocess
+import time
+import glob
+import sys
+import ConfigParser
+import re
+import shutil
+from optparse import OptionParser
+from command_utility import utility
+from config_utility import conf_utility
+
+
+conf = conf_utility() 
+dir = conf.getDirectoryPath()
+data = conf.getDataFilePath()
+adapter = conf.getAdapterPath()
+bin = conf.getBinFilePath()
+userinfo = conf.getUserInfo()
+
+# args
+##############
+USER = userinfo['name']
+util = utility(USER) 
+
+LOGDIR= dir['log']
+util.check_mkdir(LOGDIR)
+
+qsub = ['qsub']
+qsub.extend(['-e', LOGDIR, '-o', LOGDIR])
+
+# INTERVAL = dir['db'] + '/' + options.interval
+# BAIT_NUM = util.checkFileNum(INTERVAL)
+
+# realign  
+##########################
+jobIDs = []
+cmd = ['-l' ,'s_vmem=8G,mem_req=8', \
+dir['script'] + '/my_gatkRealign_setup.sh', \
+data['hg19fasta'], \
+bin['gatk'], \
+bin['java6'], \
+dir['tmp'], \
+dir['script']]
+jobIDs.append(util.qsub_cmd(cmd,qsub))
+util.syncbarrier('gatk realign',jobIDs)
+
